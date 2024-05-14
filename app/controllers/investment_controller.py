a003f22f933fb8157f8d17552ebd5e8861ba1abd
Ricardo Rodriguez <r.rodriguezreyes@outlook.com>
2024-05-14 10:26:37 -0400

    update al archivo de requirements.txt

--- app/controllers/investment_controller.py
+++ app/controllers/investment_controller.py
@@ -1,4 +1,5 @@
 # app/api/controllers/investment_controller.py
+from datetime import datetime
 
 from fastapi import APIRouter, HTTPException
 from app.models.investment import InvestmentRequest, InvestmentResponse
@@ -9,6 +10,15 @@ router = APIRouter()
 
 @router.post("/investment-dates", response_model=InvestmentResponse)
 async def calculate_investment(request: InvestmentRequest):
-        investment_dates = await calculate_investment_dates(request)
-        return investment_dates
+        try:
+                # Parsear la fecha de creación
+                fecha_inicio = datetime.strptime(request.fecha_creacion, "%Y-%m-%d %H:%M:%S")
+
+                # Calcular las fechas de inversión
+                investment_dates = await calculate_investment_dates(request)
+
+                return investment_dates
+
+        except ValueError as ve:
+                raise HTTPException(status_code=400, detail=str(ve))
 
